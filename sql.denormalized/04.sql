3cca81f81718e73b2676874d0105ff9ac7314578
Emily <efernandez23@cmc.edu>
2023-05-05 11:53:07 -0700

    added files

---
+++ sql.denormalized/04.sql
@@ -0,0 +1,8 @@
+SELECT 
+    count(DISTINCT data->>'id')
+FROM 
+    tweets_jsonb
+WHERE 
+    data->>'lang' = 'en'
+    AND
+    to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text', data->>'text')) @@ to_tsquery('english','coronavirus')
